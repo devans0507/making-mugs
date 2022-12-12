3d055da1f97963d57ee9e911ccb47cb4943747c7
Daniel Evans <evans02d@gmail.com>
2022-12-12 10:01:55 -0600

    mugs

---
+++ clay coffee cup.js
@@ -0,0 +1,11 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const cup of clay) {
+   const mug = `${cup} coffee mug`
+toFireInKiln.push(mug)
+}
+
+
+console.log(toFireInKiln)
+
